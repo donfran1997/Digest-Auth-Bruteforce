d7a2993ba8241e87d503bdd002aa0ff9163405da
donfran1997 <donfran@DESKTOP-5R364RQ.localdomain>
2020-12-07 15:56:45 +1100

    more and more magic

--- Digest.py
+++ Digest.py
@@ -1,18 +1,18 @@
 import hashlib
 
-with open("/mnt/c/Tools/SecLists-master/Passwords/darkweb2017-top1000.txt","r") as file1:
+with open("replace","r") as file1:
         passwordL = [line.rstrip() for line in file1] 
 
-response="2cf88601becb070e2ea76fcfcda0e1fc"
+response="replace"
 
 for i in passwordL:
     authstr = "master:ManagementRealm:" + i
     h1=hashlib.md5(authstr).hexdigest()
-    h2=hashlib.md5("GET:/management").hexdigest()
-    nonce="AAAABQAAZsO0/W9fbE7WPM++6W44kTkERQsD+a9khhgtpzxJhekcN3956R4="
-    cnonce="36978ce4c3368ab0"
-    nc="00000001"
-    qop="auth"
+    h2=hashlib.md5("replace").hexdigest()
+    nonce="replace"
+    cnonce="replace"
+    nc="replace"
+    qop="replace"
     rs = h1 + ":" + nonce + ":" + nc + ":" + cnonce + ":" + qop + ":" + h2
     response1=hashlib.md5(rs).hexdigest()
     if response1 == response:
